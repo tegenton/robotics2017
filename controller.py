9492a8768b6683ee05187d309d15cef2b432d880
tegenton <tegenton@ucvts.tec.nj.us>
2017-12-18 13:51:46 -0500

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,22 @@
+import radio
+import random
+from microbit import *
+
+# The radio won't work unless it's switched on.
+radio.on()
+
+//define directional signals
+forward = 
+backward =
+left =
+right = 
+
+while True:
+  # if forward
+    radio.send(forward)
+  # if backward
+    radio.send(backward)
+  # if left
+    radio.send(left)
+  # if forward
+    radio.send(right)
